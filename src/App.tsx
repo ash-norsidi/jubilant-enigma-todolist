516f0e3e36538fe017f50ecf64111a9b8e080799
Ashi <ash.norsidi@gmail.com>
2025-08-02 00:23:56 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,20 @@
+import { useState } from 'react';
+import { TodoInput, TodoList } from './components';
 
+function App() {
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const addTodo = (newTodo: string) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <TodoInput onAddTodo={addTodo} />
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default App;
