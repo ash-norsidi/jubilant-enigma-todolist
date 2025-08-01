2ce78e07585f448ed31993e1654c39f70ab3223c
Ashi <ash.norsidi@gmail.com>
2025-08-02 00:37:20 +0800

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,1 +1,17 @@
+import TodoItem from './TodoItem';
 
+type TodoListProps = {
+  todos: string[];
+};
+
+function TodoList({ todos }: TodoListProps) {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <TodoItem key={index} text={todo} />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
